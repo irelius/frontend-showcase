ec2d6b6bef94f0c95fea6b77fb19c9626f481ccf
irelius <kihong711@hotmail.com>
2024-09-24 17:38:45 -0700

    initial setup finished

--- src/error-page.jsx
+++ src/error-page.jsx
@@ -1,13 +1,25 @@
+import { useContext } from "react";
 import { useRouteError } from "react-router-dom";
+import { ThemeContext } from "./context/ThemeContext";
 
 export default function ErrorPage() {
 	const error = useRouteError();
+	const { theme } = useContext(ThemeContext);
+
 	return (
-		<div id="error-page">
+		<div id="error-page" className="error-page-container dfc jcc aic">
 			<p>Sorry, an unexpected error has occurred.</p>
 			<p>
-				<i>{error.statusText || error.message}</i>
+				<i>Error: {error.statusText || error.message}</i>
 			</p>
+			<button
+				className={`error-page-button dfr jcc border ${theme}-border pointer`}
+				onClick={() => {
+					history.back();
+				}}
+			>
+				Go Back
+			</button>
 		</div>
 	);
 }

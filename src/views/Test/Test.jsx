2e8605b366a15ba0e9dc202f3ee4f12ed416c0ea
irelius <kihong711@hotmail.com>
2024-10-21 06:49:11 -0700

    weather api intro

--- src/views/Test/Test.jsx
+++ src/views/Test/Test.jsx
@@ -1,157 +1,137 @@
 import "./Test.css";
 
+import { fetchWeatherApi } from "openmeteo";
 import { useContext, useEffect, useState } from "react";
-import { ThemeContext } from "../../context/ThemeContext";
-import LoadingAnimation from "../../components/LoadingAnimation/LoadingAnimation";
+import { LoadingAnimation } from "../../components";
 
 function Test() {
-	const { theme, changeTheme } = useContext(ThemeContext);
+	// const asdf = {
+	// 	"--d": "black",
+	// };
 
-	const asdf = {
-		"--d": "black",
-	};
-
+	const [responses, setResponses] = useState(null);
 	const [load, setLoad] = useState(false);
-	const [test, setTest] = useState(false);
+	const [userLocation, setUserLocation] = useState({
+		lat: null,
+		long: null,
+	});
+
+	const url = "https://api.open-meteo.com/v1/forecast";
+	const [params, setParams] = useState({
+		latitude: null,
+		longitude: null,
+		current: null,
+		hourly: null,
+		daily: null,
+	});
 
 	useEffect(() => {
-		setTimeout(() => {
-			setLoad(true);
-		}, 2000);
+		// get current user's location
+
+		const success = (position) => {
+			setUserLocation((prev) => ({
+				...prev,
+				lat: position.coords.latitude,
+				long: position.coords.longitude,
+			}));
+		};
+
+		const fail = () => {
+			setUserLocation((prev) => ({
+				...prev,
+				lat: 40.7128,
+				long: 74.006,
+			}));
+		};
+
+		navigator.geolocation.getCurrentPosition(success, fail);
 	}, []);
 
 	useEffect(() => {
-		if (load) {
-			setTimeout(() => {
-				setTest(true);
-			}, 250);
+		if (userLocation.lat && userLocation.long) {
+			setLoad(true);
 		}
-	}, [load]);
+		console.log("booba", userLocation);
+	}, [userLocation]);
+
+	// useEffect(() => {
+	// 	const temp = async () => {
+	// 		const data = await fetchWeatherApi(url, params);
+	// 		setResponses(data);
+	// 		setTest(true);
+	// 	};
+
+	// 	temp();
+	// }, []);
+
+	// useEffect(() => {
+	// 	if (test) {
+	// 		// Helper function to form time ranges
+	// 		const range = (start, stop, step) =>
+	// 			Array.from({ length: (stop - start) / step }, (_, i) => start + i * step);
+
+	// 		// Process first location. Add a for-loop for multiple locations or weather models
+	// 		const response = responses[0];
+
+	// 		// Attributes for timezone and location
+	// 		const utcOffsetSeconds = response.utcOffsetSeconds();
+	// 		const timezone = response.timezone();
+	// 		const timezoneAbbreviation = response.timezoneAbbreviation();
+	// 		const latitude = response.latitude();
+	// 		const longitude = response.longitude();
+
+	// 		const current = response.current();
+	// 		const hourly = response.hourly();
+	// 		const daily = response.daily();
+
+	// 		// Note: The order of weather variables in the URL query and the indices below need to match!
+	// 		const weatherData = {
+	// 			current: {
+	// 				time: new Date((Number(current.time()) + utcOffsetSeconds) * 1000),
+	// 				temperature: current.variables(0).value(), // Current is only 1 value, therefore `.value()`
+	// 				weatherCode: current.variables(1).value(),
+	// 				windSpeed: current.variables(2).value(),
+	// 				windDirection: current.variables(3).value(),
+	// 			},
+	// 			hourly: {
+	// 				time: range(Number(hourly.time()), Number(hourly.timeEnd()), hourly.interval()).map(
+	// 					(t) => new Date((t + utcOffsetSeconds) * 1000)
+	// 				),
+	// 				temperature: hourly.variables(0).valuesArray(), // `.valuesArray()` get an array of floats
+	// 				precipitation: hourly.variables(1).valuesArray(),
+	// 			},
+	// 			daily: {
+	// 				time: range(Number(daily.time()), Number(daily.timeEnd()), daily.interval()).map(
+	// 					(t) => new Date((t + utcOffsetSeconds) * 1000)
+	// 				),
+	// 				weatherCode: daily.variables(0).valuesArray(),
+	// 				temperatureMax: daily.variables(1).valuesArray(),
+	// 				temperatureMin: daily.variables(2).valuesArray(),
+	// 			},
+	// 		};
+
+	// 		// `weatherData` now contains a simple structure with arrays for datetime and weather data
+	// 		for (let i = 0; i < weatherData.daily.time.length; i++) {
+	// 			console.log(
+	// 				weatherData.daily.time[i].toISOString(),
+	// 				weatherData.daily.weatherCode[i],
+	// 				weatherData.daily.temperatureMax[i],
+	// 				weatherData.daily.temperatureMin[i]
+	// 			);
+	// 		}
+	// 	}
+	// }, [test]);
 
 	return load ? (
-		<div className={`test-${test}`}>
-            <section>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-					ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-					ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-					reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
-					sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-					est laborum.
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-					ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-					ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-					reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
-					sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-					est laborum.
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-					ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-					ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-					reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
-					sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-					est laborum.
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-					ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-					ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-					reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
-					sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-					est laborum.
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-					ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-					ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-					reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
-					sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-					est laborum.
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-					ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-					ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-					reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
-					sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-					est laborum.
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-					ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-					ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-					reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
-					sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-					est laborum.
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-					ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-					ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-					reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
-					sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-					est laborum.
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-					ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-					ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-					reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
-					sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-					est laborum.
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-					ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-					ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-					reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
-					sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-					est laborum.
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-					ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-					ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-					reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
-					sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-					est laborum.
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-					ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-					ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-					reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
-					sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-					est laborum.
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-					ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-					ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-					reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
-					sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-					est laborum.
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-					ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-					ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-					reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
-					sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-					est laborum.
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-					ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-					ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-					reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
-					sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-					est laborum.
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-					ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-					ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-					reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
-					sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-					est laborum.
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-					ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-					ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-					reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
-					sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-					est laborum.
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-					ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-					ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-					reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
-					sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-					est laborum.
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-					ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-					ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-					reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
-					sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-					est laborum.
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-					ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-					ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-					reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
-					sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-					est laborum.
-            </section>
+		<div>
+			<label>First name:</label>
+			<input type="text" />
+			<label>Last name:</label>
+			<input type="text" />
+			<section>
+				<section>{userLocation.lat}</section>
+				<section>{userLocation.long}</section>
+			</section>
 		</div>
 	) : (
 		<LoadingAnimation />

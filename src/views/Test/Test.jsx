6b6478618a5d922e0b863c2fab50d27e60acaacc
irelius <kihong711@hotmail.com>
2024-11-05 00:03:21 -0800

    data flow for weather data request setup

--- src/views/Test/Test.jsx
+++ src/views/Test/Test.jsx
@@ -1,140 +1,152 @@
 import "./Test.css";
 
-import { fetchWeatherApi } from "openmeteo";
 import { useContext, useEffect, useState } from "react";
-import { LoadingAnimation } from "../../components";
+import Lottie from "lottie-react";
+import weatherLoadingAnimation from "../../assets/lottie/weather-loading.json";
 
 function Test() {
-	// const asdf = {
-	// 	"--d": "black",
-	// };
+	const weatherApi = import.meta.env.VITE_WEATHER_API;
+	const savedLocation = localStorage.getItem("savedLocation") || "null";
 
-	const [responses, setResponses] = useState(null);
-	const [load, setLoad] = useState(false);
-	const [userLocation, setUserLocation] = useState({
-		lat: null,
-		long: null,
-	});
+	// location from geolocation request
+	const [location, setLocation] = useState("");
 
-	const url = "https://api.open-meteo.com/v1/forecast";
-	const [params, setParams] = useState({
-		latitude: null,
-		longitude: null,
-		current: null,
-		hourly: null,
-		daily: null,
-	});
+	// location from input field
+	const [locationInput, setLocationInput] = useState("");
 
-	useEffect(() => {
-		// get current user's location
+	// trigger variable to know when to send request to fetch weather data
+	const [locationProvided, setLocationProvided] = useState(false);
 
-		const success = (position) => {
-			setUserLocation((prev) => ({
+	const [weatherData, setWeatherData] = useState(null);
+	const [error, setError] = useState({});
+
+	// method to get data from weather api
+	const fetchWeather = async () => {
+		// Send request using the params provided
+		setLocation(locationInput);
+
+		const data = await fetch(
+			`http://api.weatherapi.com/v1/current.json?key=${weatherApi}&q=${location}&aqi=no`
+		).then((res) => res.json());
+
+		if ("error" in data) {
+			setError((prev) => ({
 				...prev,
-				lat: position.coords.latitude,
-				long: position.coords.longitude,
+				...data.error,
 			}));
+			setWeatherData(null);
+		} else {
+			setError({});
+			setWeatherData((prev) => ({
+				...prev,
+				...data.current,
+				...data.location,
+			}));
+
+			const preppedLocation = `${data.location.name}, ${data.location.region}`;
+			localStorage.setItem("savedLocation", preppedLocation);
+			setLocationInput(preppedLocation);
+		}
+	};
+
+	// get user's current location, returns lat and lng
+	const getCurrentPosition = async () => {
+		const success = async (position) => {
+			setLocation(`${position.coords.latitude}, ${position.coords.longitude}`);
+			setLocationProvided((prev) => !prev);
 		};
 
 		const fail = () => {
-			setUserLocation((prev) => ({
-				...prev,
-				lat: 40.7128,
-				long: 74.006,
-			}));
+			setLocation("");
 		};
 
-		navigator.geolocation.getCurrentPosition(success, fail);
+		const permission = await navigator.permissions.query({ name: "geolocation" });
+
+		if (permission.state === "denied") {
+			window.alert(
+				`Looks like we don't have permission to get your location. Please reset the permissions and try again.\n\nAlternatively, manually input your location (e.g. city, zip code).
+                `
+			);
+		} else {
+			// ask for permission to get user's current location
+			navigator.geolocation.getCurrentPosition(success, fail);
+		}
+	};
+
+	useEffect(() => {
+		const setupStorage = async () => {
+			const permission = await navigator.permissions.query({ name: "geolocation" });
+
+			if (permission.state === "prompt") {
+				localStorage.setItem("savedLocation", "null");
+				getCurrentPosition();
+			} else if (permission.state === "granted") {
+				if (savedLocation === "null") {
+					getCurrentPosition();
+				} else {
+					setLocation(savedLocation);
+				}
+				setLocationProvided((prev) => !prev);
+			}
+		};
+
+		setupStorage();
 	}, []);
 
 	useEffect(() => {
-		if (userLocation.lat && userLocation.long) {
-			setLoad(true);
-		}
-		console.log("booba", userLocation);
-	}, [userLocation]);
+		fetchWeather();
+	}, [locationProvided]);
 
-	// useEffect(() => {
-	// 	const temp = async () => {
-	// 		const data = await fetchWeatherApi(url, params);
-	// 		setResponses(data);
-	// 		setTest(true);
-	// 	};
-
-	// 	temp();
-	// }, []);
-
-	// useEffect(() => {
-	// 	if (test) {
-	// 		// Helper function to form time ranges
-	// 		const range = (start, stop, step) =>
-	// 			Array.from({ length: (stop - start) / step }, (_, i) => start + i * step);
-
-	// 		// Process first location. Add a for-loop for multiple locations or weather models
-	// 		const response = responses[0];
-
-	// 		// Attributes for timezone and location
-	// 		const utcOffsetSeconds = response.utcOffsetSeconds();
-	// 		const timezone = response.timezone();
-	// 		const timezoneAbbreviation = response.timezoneAbbreviation();
-	// 		const latitude = response.latitude();
-	// 		const longitude = response.longitude();
-
-	// 		const current = response.current();
-	// 		const hourly = response.hourly();
-	// 		const daily = response.daily();
-
-	// 		// Note: The order of weather variables in the URL query and the indices below need to match!
-	// 		const weatherData = {
-	// 			current: {
-	// 				time: new Date((Number(current.time()) + utcOffsetSeconds) * 1000),
-	// 				temperature: current.variables(0).value(), // Current is only 1 value, therefore `.value()`
-	// 				weatherCode: current.variables(1).value(),
-	// 				windSpeed: current.variables(2).value(),
-	// 				windDirection: current.variables(3).value(),
-	// 			},
-	// 			hourly: {
-	// 				time: range(Number(hourly.time()), Number(hourly.timeEnd()), hourly.interval()).map(
-	// 					(t) => new Date((t + utcOffsetSeconds) * 1000)
-	// 				),
-	// 				temperature: hourly.variables(0).valuesArray(), // `.valuesArray()` get an array of floats
-	// 				precipitation: hourly.variables(1).valuesArray(),
-	// 			},
-	// 			daily: {
-	// 				time: range(Number(daily.time()), Number(daily.timeEnd()), daily.interval()).map(
-	// 					(t) => new Date((t + utcOffsetSeconds) * 1000)
-	// 				),
-	// 				weatherCode: daily.variables(0).valuesArray(),
-	// 				temperatureMax: daily.variables(1).valuesArray(),
-	// 				temperatureMin: daily.variables(2).valuesArray(),
-	// 			},
-	// 		};
-
-	// 		// `weatherData` now contains a simple structure with arrays for datetime and weather data
-	// 		for (let i = 0; i < weatherData.daily.time.length; i++) {
-	// 			console.log(
-	// 				weatherData.daily.time[i].toISOString(),
-	// 				weatherData.daily.weatherCode[i],
-	// 				weatherData.daily.temperatureMax[i],
-	// 				weatherData.daily.temperatureMin[i]
-	// 			);
-	// 		}
-	// 	}
-	// }, [test]);
-
-	return load ? (
+	return (
 		<div>
-			<label>First name:</label>
-			<input type="text" />
-			<label>Last name:</label>
-			<input type="text" />
+			<button
+				onClick={() => {
+					setLocation(locationInput);
+					setLocationProvided((prev) => !prev);
+				}}
+			>
+				Get Weather Data
+			</button>
+			<button onClick={() => getCurrentPosition()}>Get Your Location</button>
 			<section>
-				<section>{userLocation.lat}</section>
-				<section>{userLocation.long}</section>
+				<input
+					type="text"
+					placeholder="Provide your location"
+					value={locationInput}
+					onChange={(e) => setLocationInput(e.target.value)}
+					onKeyUp={(e) => {
+						if (e.key === "Enter") {
+							setLocation(locationInput);
+							setLocationProvided((prev) => !prev);
+						}
+					}}
+				/>
 			</section>
+			{weatherData ? (
+				<section>
+					<aside>Condition: {weatherData.condition.text}</aside>
+					<aside>
+						<img src={weatherData.condition.icon} />
+					</aside>
+					<aside>Cloud: {weatherData.cloud}</aside>
+					<aside>Temperature: {weatherData.temp_f}</aside>
+					<aside>Feels Like: {weatherData.feelslike_f}</aside>
+					<aside>UV: {weatherData.uv}</aside>
+					<aside>Humidity: {weatherData.humidity}</aside>
+				</section>
+			) : (
+				// Error section
+				<section>
+					{error.code === 1006 ? (
+						<section>Whoops, looks like the location you provided doesn't exist.</section>
+					) : error.code === 1003 ? (
+						<section>Whoops, looks like you forgot to provide your location.</section>
+					) : (
+						<></>
+					)}
+				</section>
+			)}
 		</div>
-	) : (
-		<LoadingAnimation />
 	);
 }
 
